7195a4855bf0cd758a78589693c8ca506f9b56a7
Marianne <109239971+dillardm89@users.noreply.github.com>
2023-11-14 23:16:52 -0500

    Update fanprogram.py
    corrected formatting + syntax

--- CeilingFan/fanprogram.py
+++ CeilingFan/fanprogram.py
@@ -1,10 +1,20 @@
-import sys, time
-from fanfunctions import *
+import sys
+import time
+from fanfunctions import create_fan
+from fanfunctions import display_fans_by_id
+from fanfunctions import delete_fan
+from fanfunctions import create_fan_list
+from fanfunctions import select_fan_by_id
+from fanfunctions import view_fan_details
+from fanfunctions import modify_fan_choice
 
 
-#Function to create main menu
-def mainMenu ():
-    print("What would you like to do?:\n1: Create new fan\n2: Delete existing fan\n3: View list of fans\n4: View specific fan details\n5: Modify fan (change status, speed, direction)\n6: Exit program")
+# Function to create main menu
+def main_menu():
+    menu_text = "What would you like to do?:\n1: Create new fan\n2: Delete " +\
+        "existing fan\n3: View list of fans\n4: View specific fan details\n" +\
+        "5: Modify fan (change status, speed, direction)\n6: Exit program"
+    print(menu_text)
 
     selection = input("Enter the operation number: ")
 
@@ -12,63 +22,63 @@ def mainMenu ():
         print("Please enter a valid number selection.")
         selection = input("Enter a valid operation number: ")
 
-    #Create new fan
+    # Create new fan
     if selection == "1":
-        createFan()
+        create_fan()
         return_to_menu()
 
-    #Delete existing fan
+    # Delete existing fan
     elif selection == "2":
-        fanID = displayFansbyID(selection)
-        deleteFan(fanID)
+        fan_id = display_fans_by_id(selection)
+        delete_fan(fan_id)
         return_to_menu()
 
-    #View list of fans
+    # View list of fans
     elif selection == "3":
         print("Below is the current fan list.")
-        fanIDList = createFanList()
-        
+        fan_id_list = create_fan_list()
+
         choice = input("Would you like to view details for a specific fan? (Y / N) ")
         while choice.upper() not in ("Y", "N"):
             print("Please enter a valid selection.")
             choice = input("Would you like to view details for a specific fan? (Y / N) ")
-        
+
         if choice.upper() == "N":
             return_to_menu()
         else:
-            fanID = selectFanbyID(selection, fanIDList)
-            viewFanDetails(fanID)
+            fan_id = select_fan_by_id(selection, fan_id_list)
+            view_fan_details(fan_id)
             return_to_menu()
 
-    #View specific fan details
+    # View specific fan details
     elif selection == "4":
-        fanID = displayFansbyID(selection)
-        viewFanDetails(fanID)
+        fan_id = display_fans_by_id(selection)
+        view_fan_details(fan_id)
         return_to_menu()
 
-    #Modify fan (change status, speed, direction)
+    # Modify fan (change status, speed, direction)
     elif selection == "5":
-        fanID = displayFansbyID(selection)
-        modifyFan(fanID)
+        fan_id = display_fans_by_id(selection)
+        modify_fan_choice(fan_id)
         return_to_menu()
 
-    #Exit program
+    # Exit program
     else:
-        exitProgram()
+        exit_program()
 
 
-#Function to return to menu
+# Function to return to menu
 def return_to_menu():
     input("Press 'Enter' key to return to the main menu.")
-    mainMenu()
-    
+    main_menu()
 
-#Function to exit program
-def exitProgram():
+
+# Function to exit program
+def exit_program():
     print("Exiting...")
     time.sleep(1)
     sys.exit()
 
 
 if __name__ == "__main__":
-    mainMenu()
+    main_menu()
